89f72b1781fcc02dcc984740bee64786c298ccf2
Julian Montes <julianmontesPS4@gmail.com>
2023-08-20 19:23:37 -0500

    van details done

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = useState([]);
@@ -11,21 +12,26 @@ const Vans = () => {
   }, []);
 
   return (
-    <>
-      {vans.map((van) => (
-        <div key={van.id} className="van-tile">
-          <img src={van.imageUrl} />
-          <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>
-              ${van.price}
-              <span>/day</span>
-            </p>
+    <div className="van-list-container">
+      <h1>Explore our options</h1>
+      <div className="van-list">
+        {vans.map((van) => (
+          <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`}>
+              <img src={van.imageUrl} />
+              <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>
+                  ${van.price}
+                  <span>/day</span>
+                </p>
+              </div>
+              <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
           </div>
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 
